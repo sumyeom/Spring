54af2240ddf6d5e8c3372eff1010514980d27407
sumyeom <wjstndus75@naver.com>
2024-11-05 18:00:49 +0900

    [Spring_Memo] Memo 실습2
    1. 기능 추가

--- src/main/java/com/example/memo/controller/MemoController.java
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -3,11 +3,11 @@ package com.example.memo.controller;
 import com.example.memo.dto.MemoRequestDto;
 import com.example.memo.dto.MemoResponseDto;
 import com.example.memo.entitiy.Memo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/memos") //prefix
@@ -15,7 +15,7 @@ public class MemoController {
     private final Map<Long, Memo> memoList = new HashMap<>();
 
     @PostMapping
-    public MemoResponseDto createMemo(@RequestBody MemoRequestDto dto){
+    public ResponseEntity<MemoResponseDto> createMemo(@RequestBody MemoRequestDto dto){
         // 식별자가 1씩 증가하도록 만듦
         Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
 
@@ -25,32 +25,90 @@ public class MemoController {
         //inmemory DB 에 Memo 저장
         memoList.put(memoId, memo);
 
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.CREATED);
     }
 
+    @GetMapping
+    public List<MemoResponseDto> findAllMemos(){
+
+        // init List
+        List<MemoResponseDto> responseList = new ArrayList<>();
+
+        // HashMap<Memo> -> List<MemoResponseDto>
+        for(Memo memo : memoList.values()){
+            MemoResponseDto responseDto = new MemoResponseDto(memo);
+            responseList.add(responseDto);
+        }
+
+        // Map to List
+        // responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
+        return responseList;
+    }
     @GetMapping("/{id}")
-    public MemoResponseDto findMemoById(@PathVariable Long id){
+    public ResponseEntity<MemoResponseDto> findMemoById(@PathVariable Long id){
         Memo memo = memoList.get(id);
 
-        return new MemoResponseDto(memo);
+        // 조회된 메모가 없다면 not found 리턴
+        if(memo == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public MemoResponseDto udpateMemoById(
+    public ResponseEntity<MemoResponseDto> updateMemoById(
             @PathVariable Long id,
             @RequestBody MemoRequestDto dto
     ){
         Memo memo = memoList.get(id);
 
+        if(memo == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        if(dto.getTitle() == null || dto.getContents() == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         memo.update(dto);
 
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<MemoResponseDto> updateTitle(
+            @PathVariable Long id,
+            @RequestBody MemoRequestDto dto
+    ){
+        Memo memo = memoList.get(id);
+
+        // NPE 방지
+        if(memo == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        // 필수값 검증
+        if(dto.getTitle() == null || dto.getContents() != null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        memo.updateTitle(dto);
+
+        return new ResponseEntity<>(new MemoResponseDto(memo),HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteMemo(
+    public ResponseEntity<Void> deleteMemo(
             @PathVariable Long id
     ){
-        memoList.remove(id);
+        // memoList의 Key값에 id를 포함하고 있다면
+        if(memoList.containsKey(id)){
+            //key가 id인 value삭제
+            memoList.remove(id);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
